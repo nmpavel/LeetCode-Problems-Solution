af7ffff26b871776d3afeb65038706484fcea2fd
Nur Mohammad Pavel <53833983+nmpavel@users.noreply.github.com>
2023-05-27 15:27:06 +0600

    Create MinStack-155.cpp
    Medium type problem, Basic Stack Related

---
+++ MinStack-155.cpp
@@ -0,0 +1,44 @@
+class MinStack {
+public:
+    stack<int>st;
+    stack<int>minSt;
+
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+     if(st.empty()){
+         st.push(val);
+         if(minSt.empty()){
+             minSt.push(val);
+         }
+     }else{
+         st.push(val);
+         int minVal = min(val,minSt.top());
+         minSt.push(minVal);
+     }
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+       return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
